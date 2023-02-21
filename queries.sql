a12a1e761e5804e021440534d08432a7d1ae6476
Anthony Luong <anthony@Anthonys-MacBook-Pro.local>
2023-02-21 17:59:14 -0500

    Troubleshoot Part 2 EOD Skills

--- queries.sql
+++ queries.sql
@@ -1,7 +1,14 @@
 -- Part 1: Test it with SQL
-
+--columns | datatype
+--id INT(11)
+--employer VARCHAR(255)
+--name VARCHAR(255)
+--skills VARCHAR(255)
+--Look in Field Types on the Right Side
 -- Part 2: Test it with SQL
-
+SELECT name
+FROM employer
+WHERE location="St.Louis";
 -- Part 3: Test it with SQL
 
 -- Part 4: Test it with SQL
---
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -0,0 +1,63 @@
+package org.launchcode.techjobs.persistent.controllers;
+
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+@Controller
+@RequestMapping("skills")
+public class SkillController {
+
+    //    Add a private field of EmployerRepository type called employerRepository to EmployerController.✅
+//    Give this field an @Autowired annotation.✅
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping
+    public String displayAllSkills(Model model){
+        model.addAttribute("title", "All Skills");
+        model.addAttribute("skills", skillRepository.findAll());
+        return "skills/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddSkillForm(Model model) {
+        model.addAttribute(new Skill());
+        return "skills/add";
+    }
+
+    @PostMapping("add")
+    public String processAddSkillForm(@ModelAttribute @Valid Skill newSkill,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "skills/add";
+        }
+        skillRepository.save(newSkill);
+        return "redirect:";
+    }
+    // Use the employerRepository to save our newEmployer to the database✅
+
+    @GetMapping("view/{skillId}")
+    public String displayViewSkill(Model model, @PathVariable int skillId) {
+
+        // use the findById method of the employerRepository along with the
+        // employerId parameter to get a specific single employer from the database✅
+        Optional optSkill = skillRepository.findById(skillId);
+        if (optSkill.isPresent()) {
+            Skill skill = (Skill) optSkill.get();
+            model.addAttribute("skill", skill);
+            return "skills/view";
+        } else {
+            return "redirect:../";
+        }
+    }
+}
