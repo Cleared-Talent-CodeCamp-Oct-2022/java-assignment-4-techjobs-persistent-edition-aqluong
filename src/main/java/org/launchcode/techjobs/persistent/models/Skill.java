4e3c3a39a82e14065f11019633d37bd4a5a83657
Anthony Luong <anthony@Anthonys-MacBook-Pro.local>
2023-02-21 22:36:37 -0500

    Late night Code edits

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -9,10 +9,9 @@ public class Skill extends AbstractEntity {
 
     //    add a field for a longer description of the skill, named description, ✅
 //    with public accessor methods.✅
-    @Size(min = 1, max = 100, message = "Please keep descriptons under 100 characters.")
+    @Size(min = 1, max = 100, message = "Please keep descriptions under 100 characters.")
     @NotNull(message = "Please provide a description")
     private String description;
-
     //    no-arg constructor.✅
     public Skill(){
     }
